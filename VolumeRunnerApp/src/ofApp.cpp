f711b727ddc21dae0ebdcdd8b8bd274ec6bd693e
Memo Akten <memo@memo.tv>
2014-11-24 19:31:16 +0000

    skeleton debug view

--- VolumeRunnerApp/src/ofApp.cpp
+++ VolumeRunnerApp/src/ofApp.cpp
@@ -20,9 +20,10 @@ void ofApp::setup(){
     params.addFloat("FPS").setRange(0, 60).setClamp(false);
     params.startGroup("Display"); {
         params.addBool("Debug skeleton");
+        params.addBool("Random bones");
     } params.endGroup();
     
-
+    
     params.startGroup("Shader"); {
         params.startGroup("Test box"); {
             params.addFloat("posx").setRange(-100, 100).setIncrement(1.0).setSnap(true);
@@ -35,7 +36,7 @@ void ofApp::setup(){
             params.addFloat("scaley").setRange(0, 50).setIncrement(0.1).setSnap(true);
             params.addFloat("scalez").setRange(0, 50).setIncrement(0.1).setSnap(true);
         } params.endGroup();
-
+        
         
         params.startGroup("View"); {
             params.addFloat("z").setRange(0, 100).setIncrement(1.0);
@@ -63,7 +64,7 @@ void ofApp::setup(){
     //
     animSys.addBVHFile("1",ofToDataPath("mocap/test.bvh"));
     animSys.play("1");
-//    cam = new ofEasyCam();
+    //    cam = new ofEasyCam();
 }
 
 //--------------------------------------------------------------
@@ -94,7 +95,7 @@ void ofApp::draw(){
         viewMat.rotate((params["Shader.View.rotz"]), 0, 0, 1);
         viewMat.rotate((params["Shader.View.roty"]), 0, 1, 0);
     } else {
-
+        
         wv.identity();
         wv.translate(0,0,-(float)(params["Shader.View.z"]));//-10.0);//getf('y'),-getf('z'))
         wv.rotateX(radians(params["Shader.View.rotx"]));
@@ -104,41 +105,48 @@ void ofApp::draw(){
     }
     
     // set test box transformation
-    M44 boxMat;
-    boxMat.identity();
-    boxMat.translate(params["Shader.Test box.posx"], params["Shader.Test box.posy"], params["Shader.Test box.posz"]);//radians(params["Shader.Test box.roty"]));
-    boxMat.rotateX(radians(params["Shader.Test box.rotx"]));
-    boxMat.rotateY(radians(params["Shader.Test box.roty"]));
-    boxMat.rotateZ(radians(params["Shader.Test box.rotz"]));
-    boxMat.scale(params["Shader.Test box.scalex"], params["Shader.Test box.scaley"], params["Shader.Test box.scalez"]);
-
+//    M44 boxMat;
+//    boxMat.identity();
+//    boxMat.translate(params["Shader.Test box.posx"], params["Shader.Test box.posy"], params["Shader.Test box.posz"]);//radians(params["Shader.Test box.roty"]));
+//    boxMat.rotateX(radians(params["Shader.Test box.rotx"]));
+//    boxMat.rotateY(radians(params["Shader.Test box.roty"]));
+//    boxMat.rotateZ(radians(params["Shader.Test box.rotz"]));
+//    boxMat.scale(params["Shader.Test box.scalex"], params["Shader.Test box.scaley"], params["Shader.Test box.scalez"]);
     
     
+    // read bones from animation
     std::vector<M44> boneMats = animSys.getBoneMatrices();//[kNumBones];
+    std::vector<M44> originalBones = animSys.getBoneMatrices();
+
     std::vector<float> boneLengths = animSys.getBoneLengths();
     int boneCount = min(boneMats.size(), kNumBones);
     for( int i = 0; i < boneCount; i++ )
     {
-        boneMats[i].scale(0.00001, 0.00001, 0.00001);//1,1,boneLengths[i]);
+        boneMats[i].scale(0.0000000001, 0.000000001, 0.00000001);//1,1,boneLengths[i]);
         boneMats[i].invert();
     }
     
     
-//    boneMats.clear();
-//    boneMats.resize(kNumBones);
-//    for(int i=0; i<kNumBones; i++) {
-//        M44 &m = boneMats[i];
-//        m.identity();
-//        m.translate(ofSignedNoise(i + 615.1276) * 10 + (float)params["Shader.Test box.posx"], ofSignedNoise(i + 57.137) * 10 + (float)params["Shader.Test box.posy"], ofSignedNoise(i + 12874.5) * 10 + (float)params["Shader.Test box.posz"]);
-//        m.rotateX(radians(ofSignedNoise(i + 114.1) * 360));
-//        m.rotateY(radians(ofSignedNoise(i + 572.31) * 360));
-//        m.rotateZ(radians(ofSignedNoise(i + 1767.1417) * 360));
-//        m.scale(ofNoise(i + 7162.41) * 10, ofNoise(i + 81632.41) * 10, ofNoise(i + 17626.3123) * 10);
-//        m.invert();
-//    }
+    // multiple random test bones
+    if(params["Display.Random bones"]) {
+        boneMats.clear();
+        boneMats.resize(kNumBones);
+        originalBones.resize(kNumBones);
+        for(int i=0; i<kNumBones; i++) {
+            M44 &m = boneMats[i];
+            m.identity();
+            m.translate(ofSignedNoise(i + 615.1276) * 10 + (float)params["Shader.Test box.posx"], ofSignedNoise(i + 57.137) * 10 + (float)params["Shader.Test box.posy"], ofSignedNoise(i + 12874.5) * 10 + (float)params["Shader.Test box.posz"]);
+            m.rotateX(radians(ofSignedNoise(i + 114.1) * 360));
+            m.rotateY(radians(ofSignedNoise(i + 572.31) * 360));
+            m.rotateZ(radians(ofSignedNoise(i + 1767.1417) * 360));
+            m.scale(ofNoise(i + 7162.41) * 10, ofNoise(i + 81632.41) * 10, ofNoise(i + 17626.3123) * 10);
+            originalBones[i] = m;
+            m.invert();
+        }
+    }
     
     
-
+    
     
     
     gfx::enableDepthBuffer(false);
@@ -147,14 +155,14 @@ void ofApp::draw(){
     shaderRayTracer.begin();
     shaderRayTracer.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
     shaderRayTracer.setUniform1f("time", ofGetElapsedTimef());
-
+    
     shaderRayTracer.setUniform3f("box_pos", params["Shader.Test box.posx"], params["Shader.Test box.posy"], params["Shader.Test box.posz"]);
     shaderRayTracer.setUniform3f("box_rot", ofDegToRad(params["Shader.Test box.rotx"]), ofDegToRad(params["Shader.Test box.roty"]), ofDegToRad(params["Shader.Test box.rotz"]));
     shaderRayTracer.setUniform3f("box_scale", params["Shader.Test box.scalex"], params["Shader.Test box.scaley"], params["Shader.Test box.scalez"]);
-
-//    shaderRayTracer.setUniformMatrix4f("box_mat", ofMatrix4x4((float*)boxMat).getInverse());
+    
+    //    shaderRayTracer.setUniformMatrix4f("box_mat", ofMatrix4x4((float*)boxMat).getInverse());
     shaderRayTracer.setUniformMatrix4f("box_mats", (ofMatrix4x4&) boneMats[0], kNumBones);//
-
+    
     shaderRayTracer.setUniformMatrix4f("invViewMatrix", viewMat.getInverse());//camera.getModelViewMatrix());
     shaderRayTracer.setUniform1f("tanHalfFov", tan(ofDegToRad(cam->getFov()/2)));
     //ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
@@ -171,7 +179,7 @@ void ofApp::draw(){
     gfx::setModelViewMatrix(wv);
     
     if(params["Display.Debug skeleton"]) {
-        debugDrawSkeleton(animSys.getBoneMatrices(),animSys.getBoneLengths());
+        debugDrawSkeleton(originalBones,animSys.getBoneLengths());
     }
     
     ofSetupScreen();
@@ -181,7 +189,7 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::exit(){
-//    params.saveXmlValues();
+    //    params.saveXmlValues();
     reloader->stopThread();
     delete reloader;
 }

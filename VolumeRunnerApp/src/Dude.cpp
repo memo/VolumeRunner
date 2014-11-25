c1ce680845f2ec96d1ffe8a2575ccbb7ffc0625b
Daniel Berio <drand48@gmail.com>
2014-11-25 16:27:42 +0000

    Added Dude class

---
+++ VolumeRunnerApp/src/Dude.cpp
@@ -0,0 +1,9 @@
+//
+//  Dude.cpp
+//  VolumeRunnerApp
+//
+//  Created by Daniel Berio on 11/25/14.
+//
+//
+
+#include "Dude.h"
---
+++ VolumeRunnerApp/src/Dude.h
@@ -0,0 +1,14 @@
+//
+//  Dude.h
+//  VolumeRunnerApp
+//
+//  Created by Daniel Berio on 11/25/14.
+//
+//
+
+#ifndef __VolumeRunnerApp__Dude__
+#define __VolumeRunnerApp__Dude__
+
+#include <stdio.h>
+
+#endif /* defined(__VolumeRunnerApp__Dude__) */

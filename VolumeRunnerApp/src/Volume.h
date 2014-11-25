ec10e01be468ec1ea52df089145ba3010e1fb7b1
Memo Akten <memo@memo.tv>
2014-11-25 19:13:27 +0000

    fix close params group for dude

---
+++ VolumeRunnerApp/src/Volume.h
@@ -0,0 +1,14 @@
+//
+//  Volume.h
+//  VolumeRunnerApp
+//
+//  Created by Memo Akten on 25/11/2014.
+//
+//
+
+#ifndef __VolumeRunnerApp__Volume__
+#define __VolumeRunnerApp__Volume__
+
+#include <stdio.h>
+
+#endif /* defined(__VolumeRunnerApp__Volume__) */

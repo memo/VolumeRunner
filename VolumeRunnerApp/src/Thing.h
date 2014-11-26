c12b927127a54780978870818653ff6aed1d46ef
Daniel Berio <drand48@gmail.com>
2014-11-26 17:37:49 +0000

    thing

---
+++ VolumeRunnerApp/src/Thing.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "ofMain.h"
+#include "ofxMSAControlFreak.h"
+#include "colormotor.h"
+
+class Thing
+{
+public:
+    Thing()
+    {
+        
+    }
+    
+    virtual ~Thing()
+    {
+        
+    }
+    
+    // Had to put it
+    bool innit() { return init(); }
+    
+    virtual bool init() { return false; }
+    
+    virtual void addParams( msa::controlfreak::ParameterGroup &params ) {}
+    virtual void updateParams( msa::controlfreak::ParameterGroup &params ) {}
+    
+    virtual void update() {}
+    virtual void updateRenderer( ofShader & shader ) { }
+    
+};
